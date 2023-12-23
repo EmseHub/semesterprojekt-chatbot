d495858f0fad89238899a30bc3a37158a74ea785
Emse <emsoft@gmx.net>
2023-12-23 20:34:22 +0100

    Quellen hinzugefuegt

--- rule_engine/intent_matching.py
+++ rule_engine/intent_matching.py
@@ -1,5 +1,9 @@
 def get_intent(tagged_tokens):
-    
+
     intent = {}
 
     return intent
+
+# auch "intent recognition"
+# https://botfriends.de/en/blog/botwiki/intents/
+# https://www.geeksforgeeks.org/intent-recognition-using-tensorflow/
