63bd195924dc32655f1c483813a9989c7a72eabb
Emse <emsoft@gmx.net>
2023-12-29 17:59:33 +0100

    Random-Responses

--- helpers.py
+++ helpers.py
@@ -1,6 +1,11 @@
+import random
 import json
 
 
 def parse_json_file(file_path):
     with open(file_path) as json_file:
         return json.load(json_file)
+
+
+def get_random_item_in_list(my_list):
+    return random.choice(my_list) if (my_list and isinstance(my_list, list)) else None
