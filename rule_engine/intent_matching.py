8b4c914a81425e669b23f39b6c372870f59342f5
Emse <emsoft@gmx.net>
2024-01-28 23:58:05 +0100

    Kommentare für Doku

--- rule_engine/intent_matching.py
+++ rule_engine/intent_matching.py
@@ -23,23 +23,36 @@ def get_intent(tagged_tokens):
     # Übernommen aus JS-Demo
     if not tagged_tokens or len(tagged_tokens) == 0:
         return None
+
+    # Umlaute ersetzen und Tokens in Lower Case umwandeln (man weiß nie, wie User Texte eingibt)
     tagged_tokens_no_diacritics = [
         replace_diacritics(tagged_token.lower()) for tagged_token in tagged_tokens
     ]
+
     total_words = len(tagged_tokens_no_diacritics)
     possible_intents = []
-    for obj_intent in parsed_intents:
+
+    for intent in parsed_intents:
+        # Anzhahl der Patterns des Intents, die von der Nachricht getroffen werden
         hit_count = 0
-        for str_pattern in obj_intent['patterns']:
-            if str_pattern in tagged_tokens_no_diacritics:
+        for pattern in intent['patterns']:
+            if pattern in tagged_tokens_no_diacritics:
+                # Pattern getroffen
                 hit_count += 1
-        if hit_count < obj_intent['min_hits']:
+        if hit_count < intent['min_hits']:
+            # Im Intent definiertes Minimum an Treffern nicht erreicht
             continue
         if hit_count >= total_words:
-            return {**obj_intent, 'hit_count': hit_count}
+            # Jedes Wort der Nachricht trifft -> Intent eindeutig
+            return {**intent, 'hit_count': hit_count}
         if hit_count > 0:
-            possible_intents.append({**obj_intent, 'hit_count': hit_count})
+            # Möglicherweise gemeinten Intent zur Liste hinzufügen
+            possible_intents.append({**intent, 'hit_count': hit_count})
+
     if len(possible_intents) == 0:
+        # Kein Treffer -> Default-Intent ausgeben
         return {**next(intent for intent in parsed_intents if intent['tag'] == 'trefferlos'), 'hit_count': 0}
+
+    # Intent mit den meisten Treffern ausgeben
     print('Mögliche Intents', possible_intents)
     return max(possible_intents, key=lambda intent: intent['hit_count'])
