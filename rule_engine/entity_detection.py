547b6bb08848d38a4ab42177b602a6824b7f2b26
Emse <emsoft@gmx.net>
2024-01-31 00:26:06 +0100

    Nachnamen-Erkennung vervollständigt

--- rule_engine/entity_detection.py
+++ rule_engine/entity_detection.py
@@ -43,12 +43,12 @@ test_a = [
 
 def detect_student_in_message(message_processed):
     result = {"student": None, "query": ""}
-    if not message_processed or not message_processed.strip():
+    if (not message_processed or not message_processed.strip()):
         return result
     # Prüfen, ob Nachricht Nummern enthält
     min_length_matnr = 6
     numbers_in_message = re.findall(r'\d+', message_processed)
-    if not numbers_in_message or not any(len(nr) >= min_length_matnr for nr in numbers_in_message):
+    if (not numbers_in_message or not any(len(nr) >= min_length_matnr for nr in numbers_in_message)):
         result["query"] = get_random_item_in_list([
             "Ich bräuchte noch Deine Matrikelnummer.",
             "Verrätst Du mir noch Deine Matrikelnummer?"
@@ -59,11 +59,11 @@ def detect_student_in_message(message_processed):
         student for student in students if (student["matnr"] in numbers_in_message)
     ]
     # Prüfen, ob ein oder mehrere Students gefunden wurden
-    if not matching_students:
+    if (not matching_students):
         result["query"] = "Die angegebene Zahl stimmt mit keiner Matrikelnummer überein. Gib bitte Deine vollständige Matrikelnummer an."
         return result
     # Prüfen, ob mehrere Students gefunden wurden
-    if len(matching_students) > 1:
+    if (len(matching_students) > 1):
         result["query"] = f"Welche der {len(
             matching_students)} angegebenen Matrikelnummern gehört zu Dir? Gib sie bitte erneut an."
         return result
@@ -74,14 +74,14 @@ def detect_student_in_message(message_processed):
 
 def detect_course_in_message(message_processed):
     result = {"course": None, "query": ""}
-    if not message_processed or not message_processed.strip():
+    if (not message_processed or not message_processed.strip()):
         return result
     # Prüfen, ob die Nachricht den Namen des Kurses enthält
     matching_courses = [
         course for course in courses if replace_diacritics(course["name"].lower()) in message_processed
     ]
     # Prüfen, ob ein oder mehrere Kurse gefunden wurden
-    if not matching_courses:
+    if (not matching_courses):
         result["query"] = get_random_item_in_list([
             "Wie lautet der Name des Kurses genau?",
             "Wie ist die genaue Bezeichnung des Kurses?"
@@ -256,7 +256,7 @@ def detect_address_in_message(detected_address_temp, message_raw):
         # Roh-Nachricht vergleichbar machen
         message_processed = replace_diacritics(message_raw.lower())
         # "Meine" ist eine Stadt, die hier außen vor gelassen wird und nur mit PLZ gefunden werden kann
-        message_processed = re.sub("\\bmeine", "", message_processed)
+        message_processed = re.sub(r"\bmeine", "", message_processed)
         message_processed = re.sub(r"\s+", " ", message_processed).strip()
         # Überschneidungen mit Stadtnamen aus DB finden
         matches_in_data = [
@@ -286,7 +286,7 @@ def detect_address_in_message(detected_address_temp, message_raw):
         query += " "
 
         if (not plz and not stadt):
-            query += "Verrätst Du mir auch die PLZ und den Ort, in dem du zukünftig residieren wirst?"
+            query += "Verrätst Du mir auch die PLZ und den Ort, in dem Du zukünftig residieren wirst?"
         elif (not plz):
             query += "Jetzt fehlt mir nur noch die PLZ Deiner neuen Anschrift."
         elif (not stadt):
@@ -301,6 +301,108 @@ def detect_address_in_message(detected_address_temp, message_raw):
     return {"address": detected_address_new, "query": query}
 
 
+def detect_new_surname_in_message(student, tagged_tokens, message_raw):
+    result = {"surname": None, "query": ""}
+    if (not student or not tagged_tokens or not message_raw or not message_raw.strip()):
+        return result
+
+    # Nachricht bereinigen
+    message_processed = message_raw
+    # "jetzt" entfernen
+    message_processed = re.sub(
+        r"\bjetzt\b", " ", message_processed, flags=re.IGNORECASE
+    )
+    # Jeden White Space auf ein Leerzeichen reduzieren
+    message_processed = re.sub(r"\s+", " ", message_processed).strip()
+
+    # Wert, der als Nachname in Frage kommt
+    potential_surname = None
+
+    # Prüfen, ob Nachricht einen Doppelpunkt enthält
+    if (":" in message_processed):
+        # Substring ab dem letzten Doppelpunkt bis zum nächste Satzzeichen als neuen Nachnamen auslesen
+        message_processed_splitted_by_colon = message_processed.split(":")
+        message_processed_after_colon = message_processed_splitted_by_colon[-1]
+        message_processed_after_colon_splitted_by_punctuationmarks = re.split(
+            r'([^A-Za-zÄÖÜäöüß\s-])+', message_processed_after_colon
+        )
+        potential_surname = message_processed_after_colon_splitted_by_punctuationmarks[0].strip(
+        )
+        # Falls ein Name gefunden wurde, prüfen, ob der Vorname enthalten und Nachname wirklich neu ist
+        if (potential_surname):
+            # Ggf. Vornamen entfernen
+            potential_surname = potential_surname.replace(
+                student["vorname"], ""
+            ).strip()
+
+    # Falls kein Nachname gefunden wurde, prüfe, ob die Nachricht den Vornamen mit einem anderen Nachnamen als dem bisherigen enthält
+    if (not potential_surname and student["vorname"] in message_processed):
+        message_processed_splitted_at_vorname = message_processed.split(
+            student["vorname"]
+        )
+        if (len(message_processed_splitted_at_vorname) > 1):
+            # Text, der direkt auf den Vornamen folgt, extrahieren
+            message_after_vorname = message_processed_splitted_at_vorname[-1].strip(
+            )
+            # Prüfen, ob der Text mit einem Großbuchstaben anfängt und ggf. das erste Wort als Nachnamen übernehmen
+            if (message_after_vorname and message_after_vorname[0].isupper()):
+                potential_surname = message_after_vorname.split()[0]
+                # Satzzeichen entfernen
+                potential_surname = re.sub(
+                    r"[^A-Za-zÄÖÜäöüß\s-]", "", potential_surname
+                ).strip()
+
+    # Falls kein Nachname gefunden wurde, prüfe, ob die Nachricht eine Namensanhabe in RegEx-Syntax enthält (erkennt nur Namen ohne Leerzeichen)
+    if (not potential_surname):
+        regex_match = re.search(
+            r"\b((ist|lautet|heißt|heisst|heisse|heiße|in|zu))\s+([A-ZÄÖÜ][a-zäöüß]+(-?))+",
+            message_processed,
+            re.IGNORECASE
+        )
+        if (regex_match):
+            regex_match_string = regex_match.group()
+
+            print("----regex_match_string----")
+            print(regex_match_string)
+
+            regex_match_splitted = regex_match_string.strip().split()
+            if len(regex_match_splitted) > 1:
+                potential_surname = regex_match_splitted[-1]
+
+    # Falls kein Nachname gefunden wurde, prüfe, ob es ein Token mit dem POS-Tag "NE" gibt, der nicht den bisherigen Namen entspricht
+    if (not potential_surname):
+        surname_from_token = next(
+            (tagged_token for tagged_token in tagged_tokens if (
+                tagged_token["pos"] == "NE"
+                and replace_diacritics(tagged_token["original"].lower()) != replace_diacritics(student["vorname"].lower().split()[0])
+                and replace_diacritics(tagged_token["original"].lower()) != replace_diacritics(student["nachname"].lower())
+            )),
+            None
+        )
+        if (surname_from_token):
+            potential_surname = surname_from_token["original"]
+
+    # Falls kein Nachname gefunden wurde, prüfe, ob es nur einen Token gibt, der angegeben wurde
+    if (not potential_surname and len(tagged_tokens) == 1):
+        potential_surname = tagged_tokens[0]["original"]
+
+    # Prüfen, ob der gefundene Nachname gültig und auch wirklich neu ist
+    if (potential_surname and len(potential_surname) > 1 and replace_diacritics(potential_surname.lower()) != replace_diacritics(student["nachname"].lower()) and potential_surname[0].isupper()):
+        # Satzzeichen entfernen
+        potential_surname = re.sub(
+            r"[^A-Za-zÄÖÜäöüß\s-]", "", potential_surname
+        ).strip()
+        result["surname"] = potential_surname
+    else:
+        result["query"] = get_random_item_in_list([
+            "Wie genau lautet Dein neuer Nachname?",
+            "Könntest Du nur Deinen Nachnamen bitte erneut angeben?",
+            'Gib Deinen neuen Nachnamen gerne in folgender Form an: "Mein neuer Nachname lautet: Beispielname"'
+        ])
+
+    return result
+
+
 # student_test_result = detect_student_in_message("1234567 1122334")
 # print("---student_test_result---")
 # print(student_test_result)
@@ -311,8 +413,24 @@ def detect_address_in_message(detected_address_temp, message_raw):
 # print(course_test_result)
 
 
-address_test_result = detect_address_in_message(
-    None, "Müllerweg 5 in 50737"
+# address_test_result = detect_address_in_message(None, "Müllerweg 5 in 50737")
+# print("---address_test_result---")
+# print(address_test_result)
+
+
+test_tokens = [
+    {'original': 'Fehlren', 'korrigiert': 'Fehlern', 'lemma': 'Fehler', 'pos': 'NN'},
+    {'original': 'House', 'korrigiert': 'Hose', 'lemma': 'Hose', 'pos': 'NN'},
+    {'original': 'Homer', 'korrigiert': 'Homer', 'lemma': 'Homer', 'pos': 'NE'},
+    {'original': 'Potter', 'korrigiert': 'Potter', 'lemma': 'Potter', 'pos': 'NE'}
+]
+test_tokens = [
+    {'original': 'Plotter', 'korrigiert': 'Fehlern', 'lemma': 'Fehler', 'pos': 'NN'}
+]
+new_surname_test_result = detect_new_surname_in_message(
+    students[0],
+    test_tokens,
+    "Ich heiße nicht mehr Harry James Potter, sondern mein Name ist jetzt Harry James Wattsefak"
 )
-print("---address_test_result---")
-print(address_test_result)
+print("---new_surname_test_result---")
+print(new_surname_test_result)
