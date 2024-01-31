fff16fd449888ccee734caea2201968283d37531
Emse <emsoft@gmx.net>
2024-01-31 16:42:47 +0100

    Setup/Abhängigkeiten reduziert

--- preprocessing/nltk_pipeline.py
+++ preprocessing/nltk_pipeline.py
@@ -2,60 +2,60 @@ import re
 import string
 # import nltk
 
-from nltk.corpus import stopwords
+# from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk import pos_tag, RegexpParser
+# from nltk import pos_tag, RegexpParser
 from HanTa import HanoverTagger as hanta
 
-from .spelling_correction import autocorrect_word
+from preprocessing.spelling_correction import autocorrect_word
 # from spelling_correction import autocorrect_word
 
 words_not_to_process = ['Muggel']
 hanover_tagger = hanta.HanoverTagger("morphmodel_ger.pgz")
 
 
-def get_tagged_tokens_with_ner(text_raw):
-    print("------- NER -------")
-    from nltk.chunk import tree2conlltags
+# def get_tagged_tokens_with_ner(text_raw):
+#     print("------- NER -------")
+#     from nltk.chunk import tree2conlltags
 
-    # ---Englisch---
-    clean_text = "The little yellow dog barked at the cat. Thomas likes Hollywood a lot."
-    tokens = word_tokenize(clean_text, "english")
-    tagged_tokens = pos_tag(tokens)
-    print("---tagged_tokens ENG---\n", tagged_tokens)
-    # Noun Phrase Chunking:
-    # Our chunk pattern consists of one rule, that a noun phrase (NP) should be formed whenever the chunker
-    # finds an optional determiner (DT), followed by any number of adjectives (JJ), and then a noun (NN)
-    # https://towardsdatascience.com/named-entity-recognition-with-nltk-and-spacy-8c4a7d88e7da
-    grammar = "NP: {<DT>?<JJ>*<NN>}"
-    cp = RegexpParser(grammar)
-    result = cp.parse(tagged_tokens)
-    print("---result ENG---\n", result)
-    iob_tagged = tree2conlltags(result)
-    print("---iob_tagged ENG---\n", iob_tagged)
+#     # ---Englisch---
+#     clean_text = "The little yellow dog barked at the cat. Thomas likes Hollywood a lot."
+#     tokens = word_tokenize(clean_text, "english")
+#     tagged_tokens = pos_tag(tokens)
+#     print("---tagged_tokens ENG---\n", tagged_tokens)
+#     # Noun Phrase Chunking:
+#     # Our chunk pattern consists of one rule, that a noun phrase (NP) should be formed whenever the chunker
+#     # finds an optional determiner (DT), followed by any number of adjectives (JJ), and then a noun (NN)
+#     # https://towardsdatascience.com/named-entity-recognition-with-nltk-and-spacy-8c4a7d88e7da
+#     grammar = "NP: {<DT>?<JJ>*<NN>}"
+#     cp = RegexpParser(grammar)
+#     result = cp.parse(tagged_tokens)
+#     print("---result ENG---\n", result)
+#     iob_tagged = tree2conlltags(result)
+#     print("---iob_tagged ENG---\n", iob_tagged)
 
-    # ---Deutsch---
-    clean_text = "Der kleine gelbe Hund hat die Katze angebellt."
-    tokens = word_tokenize(clean_text, "german")
-    tagged_tokens = hanover_tagger.tag_sent(tokens)
-    tagged_tokens = [
-        (tagged_token[0], tagged_token[2]) for tagged_token in tagged_tokens
-    ]
-    print("---tagged_tokens GER---\n", tagged_tokens)
-    grammar = "NP: {<ART>?<ADJ\\(A\\)>*<NN>}"
-    cp = RegexpParser(grammar)
-    result = cp.parse(tagged_tokens)
-    print("---result GER---\n", result)
-    iob_tagged = tree2conlltags(result)
-    print("---iob_tagged GER---\n", iob_tagged)
-    # Weitere Schritte erforderlich...
+#     # ---Deutsch---
+#     clean_text = "Der kleine gelbe Hund hat die Katze angebellt."
+#     tokens = word_tokenize(clean_text, "german")
+#     tagged_tokens = hanover_tagger.tag_sent(tokens)
+#     tagged_tokens = [
+#         (tagged_token[0], tagged_token[2]) for tagged_token in tagged_tokens
+#     ]
+#     print("---tagged_tokens GER---\n", tagged_tokens)
+#     grammar = "NP: {<ART>?<ADJ\\(A\\)>*<NN>}"
+#     cp = RegexpParser(grammar)
+#     result = cp.parse(tagged_tokens)
+#     print("---result GER---\n", result)
+#     iob_tagged = tree2conlltags(result)
+#     print("---iob_tagged GER---\n", iob_tagged)
+#     # Weitere Schritte erforderlich...
 
-    return (None, None)
+#     return (None, None)
 
-    # Mögliche Ansätze
-    # Flair -> https://huggingface.co/flair/ner-german-large
-    # BERT -> https://jupiter.fh-swf.de/projects/ner/ und https://huggingface.co/fhswf/bert_de_ner
-    # Electra?
+# Mögliche Ansätze
+# Flair -> https://huggingface.co/flair/ner-german-large
+# BERT -> https://jupiter.fh-swf.de/projects/ner/ und https://huggingface.co/fhswf/bert_de_ner
+# Electra?
 
 
 def get_tagged_tokens(text_raw):
@@ -74,11 +74,13 @@ def get_tagged_tokens(text_raw):
         token for token in tokens_original if token not in string.punctuation
     ]
 
+    # Nicht mehr wirklich relevant
     # Tokens, die eine Mindestanzahl an Zeichen unterschreiten, entfernen
     tokens_original = [
         token for token in tokens_original if len(token) > 0
     ]
 
+    # Keine Entfernung, das relevante Angaben enthalten sein könnten
     # Stop-Words entfernen (case-insensitive besser?) --> Stop-Words nach HanTa erneut entfernen, anhand lemmata/Korrektur?
     # tokens_original = [
     #     token for token in tokens_original if token not in stopwords.words(language)
@@ -132,9 +134,3 @@ def get_tagged_tokens(text_raw):
     ]
 
     return result_tagged_tokens
-
-
-# sample_message = "mein ist Muggel sauer"
-# sample_message = "Das ist eine Beispiel-Nachricht, Aber mit Fehlren und  Leerzeichen. Sie wurde z.B. verfasst von Dr. House und Mr. X, während der Hg. Homer ist."
-# print('---get_tagged_tokens(sample_message)----')
-# print(get_tagged_tokens(sample_message))
